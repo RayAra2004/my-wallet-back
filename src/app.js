e09a9ac6527daa29096dec5f358a9ac36953ad79
RayAra2004 <raynan.araujo@gmail.com>
2023-07-05 11:40:44 -0300

    Configurações iniciais

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try{
+    await mongoClient.connect();
+    console.log("Connected to MongoDB");
+}catch(err){
+    console.log(err.message)
+}
+
+const db = mongoClient.db();
+
+
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
